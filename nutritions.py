4704f47a23e57c9018682ceefaaa5d3105330ae8
ty928 <39279838+ty928@users.noreply.github.com>
2018-06-24 16:48:50 -0400

    Update nutritions.py

--- nutritions.py
+++ nutritions.py
@@ -7,6 +7,18 @@ import pdb
 import requests
 import datetime
 
+#CAPTURE USER INPUTS
+
+gender = input("Is a male or female: ")
+weight = int(input("Please input your weight(in pond): "))
+height = float(input("Please input your height(in feet): "))
+age = int(input("Please input your age(eg.25): "))
+
+
+if gender == "male":
+    need_calories = (10 * weight * 0.4536 + 6.25 * height *30.48 - 5 * age + 5) * 1.2
+elif gender == "female":
+    need_calories = (10 * weight * 0.4536 + 6.25 * height *30.48 - 5 * age - 161) * 1.2
 
 
 print("-------------------------------------------------")
@@ -14,8 +26,8 @@ print("Today is ", datetime.datetime.now().strftime("%Y-%B-%d"))
 print("The food recommened to you: ")
 
 
-
-nix = Nutritionix(app_id="", api_key="")
+#retrive data from API
+nix = Nutritionix(app_id="8bb7964a", api_key="7df918b6e9c42d80527477223d2fd580")
 
 obj = nix.search().nxql(
     filters={
@@ -42,14 +54,14 @@ filter_results = []
 total_calories = 0
 for result in results:
     total_calories += result["nf_calories"]
-    if total_calories <= 2000:
+    if total_calories <= need_calories:
         filter_results.append(result)
         print("Food Name: " + result['item_name'] + "........" + "Calroies: " + str(result['nf_calories']))
 
 print("-------------------------------------------------")
 print("HAVE A NICE DAY. ENJOY YOUR MEAL")
 
-
+#save to CSV
 def write_prices_to_file(hits=[], filename="data/food.csv"):
     csv_filepath = os.path.join(os.path.dirname(__file__), filename)
     with open(csv_filepath, "w") as csv_file:
@@ -65,4 +77,3 @@ def write_prices_to_file(hits=[], filename="data/food.csv"):
 food_calories = filter_results
 
 write_prices_to_file(hits= food_calories, filename="data/food.csv")
-
