c565ed662cc477e8995681f3d3537b2c63fe6379
ty928 <39279838+ty928@users.noreply.github.com>
2018-06-24 13:52:32 -0400

    Update nutritions.py

--- nutritions.py
+++ nutritions.py
@@ -9,7 +9,13 @@ import datetime
 
 
 
-nix = Nutritionix(app_id="your_app_id", api_key="your_app_key")
+print("-------------------------------------------------")
+print("Today is ", datetime.datetime.now().strftime("%Y-%B-%d"))
+print("The food recommened to you: ")
+
+
+
+nix = Nutritionix(app_id="", api_key="")
 
 obj = nix.search().nxql(
     filters={
@@ -36,9 +42,27 @@ filter_results = []
 total_calories = 0
 for result in results:
     total_calories += result["nf_calories"]
-    if total_calories <= need_calories:
+    if total_calories <= 2000:
         filter_results.append(result)
         print("Food Name: " + result['item_name'] + "........" + "Calroies: " + str(result['nf_calories']))
 
 print("-------------------------------------------------")
 print("HAVE A NICE DAY. ENJOY YOUR MEAL")
+
+
+def write_prices_to_file(hits=[], filename="data/food.csv"):
+    csv_filepath = os.path.join(os.path.dirname(__file__), filename)
+    with open(csv_filepath, "w") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=["item_name","calories"])
+        writer.writeheader()
+        for d in hits:
+            row = {
+                "item_name": d["item_name"], # change attribute name to match project requirements
+                "calories": d["nf_calories"]
+            }
+            writer.writerow(row)
+
+food_calories = filter_results
+
+write_prices_to_file(hits= food_calories, filename="data/food.csv")
+
